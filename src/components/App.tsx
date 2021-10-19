d1958763a21c542ace66b90e41df8cab66c50525
knight <knight.sarai.dev@gmail.com>
2021-10-19 20:36:52 +0300

    Add fetch Todos functionilaties

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,26 @@
+import { connect } from 'react-redux';
+import { Todo, fetchTodos } from '../actions';
+import { StoreState } from '../reducers';
+
 interface AppProps {
-  name?: string;
+  todos: Todo[];
+  fetchTodos: any;
 }
-function App(props: AppProps) {
+
+function App({ todos, fetchTodos }: AppProps) {
   return (
     <div>
-      <h1>Hello {props.name}</h1>
+      <h1>TODOS</h1>
+      <button onClick={fetchTodos}>Get Your Todos</button>
+      {todos.slice(0, 5).map((todo) => (
+        <p key={todo.id}>{todo.title}</p>
+      ))}
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+  return { todos };
+};
+
+export default connect(mapStateToProps, { fetchTodos })(App);
